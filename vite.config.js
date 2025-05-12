ecd8da4767ed1021ef438335d55772e933623b87
tprivitor <tprivitor1@gmail.com>
2025-05-12 16:22:44 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/apk-vault/',
+  base: '/Myapk-vault/',
   plugins: [react()],
 })
